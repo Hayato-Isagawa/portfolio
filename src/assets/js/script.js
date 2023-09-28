ec5d29c7022434513a791eb95d645663ed2701d4
Hayato-Isagawa <harnet0119@yahoo.co.jp>
2023-09-28 15:49:44 +0900

    section__ttlにスクロールトリガーを設置

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -43,143 +43,7 @@ for (let i = 0; i < linkElem.length; i++) {
 
 /* splash
 ============================== */
-const splash = document.querySelector('#splash');
-const splashText = document.querySelector('#splash__text');
-const space = document.querySelector('#space');
 
-var bar = new ProgressBar.Line(splash__text, {
-  easing: 'easeInOut',
-  duration: 2000,
-  strokeWidth: 6,
-  color: '#fffdf7',
-  trailWidth: 3,
-  trailColor: '#3e3e3e',
-  svgStyle: { width: '80%' },
-  text: {
-    style: {
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '-50px 0 0 0',
-      transform: 'translate(-50%, -50%)',
-      'font-size': '1.5rem',
-      color: '#fffdf7',
-      font: 'Bruno Ace',
-    },
-    autoStyleContainer: false,
-  },
-  step: function (state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + '%');
-  },
-});
-
-bar.text.style.fontFamily = '"Bruno Ace", Helvetica, sans-serif';
-
-bar.animate(1.0, function () {
-  const splashFadeOutKeyframes = {
-    opacity: [1, 0],
-  };
-  splashText.animate(splashFadeOutKeyframes, 100).finished.then(() => {
-    splashText.style.display = 'none';
-  });
-  window.warp = window.warp == 1 ? 0 : 1;
-  executeFrame();
-  setTimeout(() => {
-    splash.animate(splashFadeOutKeyframes, 300).finished.then(() => {
-      splash.style.display = 'none';
-    });
-  }, 1500);
-  setTimeout(() => {
-    stroke.play();
-  }, 3000);
-});
-
-//CodePenからの引用
-//based on an Example by @curran
-window.requestAnimFrame = (function () {
-  return window.requestAnimationFrame;
-})();
-var canvas = document.getElementById('space');
-var c = canvas.getContext('2d');
-
-var numStars = 1900;
-var radius = '0.' + Math.floor(Math.random() * 9) + 1;
-var focalLength = canvas.width * 2;
-var warp = 0;
-var centerX, centerY;
-
-var stars = [],
-  star;
-var i;
-
-var animate = true;
-
-initializeStars();
-
-function executeFrame() {
-  if (animate) requestAnimFrame(executeFrame);
-  moveStars();
-  drawStars();
-}
-
-function initializeStars() {
-  centerX = canvas.width / 2;
-  centerY = canvas.height / 2;
-
-  stars = [];
-  for (i = 0; i < numStars; i++) {
-    star = {
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      z: Math.random() * canvas.width,
-      o: '0.' + Math.floor(Math.random() * 99) + 1,
-    };
-    stars.push(star);
-  }
-}
-
-function moveStars() {
-  for (i = 0; i < numStars; i++) {
-    star = stars[i];
-    star.z--;
-
-    if (star.z <= 0) {
-      star.z = canvas.width;
-    }
-  }
-}
-
-function drawStars() {
-  var pixelX, pixelY, pixelRadius;
-
-  // Resize to the screen
-  if (canvas.width != window.innerWidth || canvas.width != window.innerWidth) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    initializeStars();
-  }
-  if (warp == 0) {
-    c.fillStyle = 'rgba(0,10,20,1)';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-  }
-  c.fillStyle = 'rgba(209, 255, 255, ' + radius + ')';
-  for (i = 0; i < numStars; i++) {
-    star = stars[i];
-
-    pixelX = (star.x - centerX) * (focalLength / star.z);
-    pixelX += centerX;
-    pixelY = (star.y - centerY) * (focalLength / star.z);
-    pixelY += centerY;
-    pixelRadius = 1 * (focalLength / star.z);
-
-    c.fillRect(pixelX, pixelY, pixelRadius, pixelRadius);
-    c.fillStyle = 'rgba(209, 255, 255, ' + star.o + ')';
-    //c.fill();
-  }
-}
-
-executeFrame();
 
 /* background
 ============================== */
@@ -705,26 +569,22 @@ $form.on('submit', function () {
 
 });
 
-
-
-
-
 gsap.registerPlugin(ScrollTrigger);
 
-
-gsap.utils.toArray('.section__ttl').forEach((el) => {
+gsap.utils.toArray('.section__ttl').forEach((el, index) => {
   const sectionTtlTl = gsap.timeline({
     scrollTrigger: {
-      trigger: 'body',
-      start: 'top top',
-      end: 'bottom bottom',
+      trigger: sections[index + 1],
+      start: 'top center+=10%',
+      end: '',
       scrub: true,
+      // markers: true
     }
   })
   sectionTtlTl.from(el, {
     x: 100,
     opacity: 0,
-    duration: 10,
+    duration: 2,
     ease: 'power4.out',
   })
 })
@@ -733,10 +593,9 @@ const topTtlTl = gsap.timeline({
   scrollTrigger: {
     trigger: '#top',
     start: 'top top',
-    // end: 'bottom bottom'
-    end: '+=800',
+    end: 'bottom bottom',
     scrub: true,
-    pin: true,
+    // pin: true,
   }
 })
 
@@ -752,7 +611,6 @@ const headerLogoTl = gsap.timeline({
     start: 'top top',
     end: '+=800',
     scrub: true,
-    pin: true,
   }
 })
 
