8c23326de57cfd0df545597b2c5f5ef32a4d0d1c
Hayato-Isagawa <harnet0119@yahoo.co.jp>
2023-10-10 10:31:11 +0900

    WordPress化に向けて、Sassファイルをまとめた。

--- src/assets/js/description.js
+++ src/assets/js/description.js
@@ -43,85 +43,8 @@ for (let i = 0; i < linkElem.length; i++) {
 /* splash
 ============================== */
 
-/* background
-============================== */
-const params__snow = {
-  particles: {
-    number: {
-      value: 100, //この数値を変更すると星の数が増減できる
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: ["#FFFFFF"],
-    },
-    shape: {
-      type: "polygon", //形状は画像を指定
-      stroke: {
-        width: 0,
-        color: ["#FFFFFF"],
-      },
-    },
-    opacity: {
-      value: 1,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 1,
-        opacity_min: 0,
-        sync: false,
-      },
-    },
-    size: {
-      value: 1,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 1,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: false,
-    },
-    move: {
-      enable: true,
-      speed: 1, //この数値を小さくするとゆっくりな動きになる
-      direction: "none", //下に向かって落ちる
-      random: true, //動きはランダム
-      straight: true, //動きをとどめない
-      out_mode: "out", //画面の外に出るように描写
-      bounce: false, //跳ね返りなし
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false,
-      },
-      onclick: {
-        enable: false,
-      },
-      resize: true,
-    },
-  },
-  retina_detect: true,
-};
-
-particlesJS("particles-js--desc", params__snow);
-
 /* header__drawer
 ============================== */
-particlesJS("drawer-background", params__snow);
 
 const hamburger = $(".hamburger__menu");
 const headerNav = $(".header__nav");
@@ -190,8 +113,7 @@ fadeInContents.forEach((fadeInContent) => {
 
 /* desc-swiper
 ------------------------------ */
-
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".desc-swiper", {
   autoplay: {
     delay: 7500,
   },
